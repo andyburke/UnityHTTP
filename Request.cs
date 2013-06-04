2cf3f24de370b3ea7350fb0f8c5c44354a0a12cb
Payne Chu <payne@piplay.com>
2013-06-04 09:53:55 +0800

    - adjusted the User-Agent to fit standard format

--- Request.cs
+++ Request.cs
@@ -274,7 +274,7 @@ namespace HTTP
             }
 
             if ( GetHeader( "User-Agent" ) == "" ) {
-                SetHeader( "User-Agent", "UnityWeb 1.0 ( Unity " + Application.unityVersion + " ) ( " + SystemInfo.operatingSystem + " )" );
+                SetHeader("User-Agent", "UnityWeb/1.0 (Unity " + Application.unityVersion + "; " + SystemInfo.operatingSystem + ")");
             }
 
             if ( GetHeader( "Connection" ) == "" ) {
